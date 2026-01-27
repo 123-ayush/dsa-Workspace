31dd13496cfb9d42c4261f303cd8c60b08375807
123-ayush <asahlawat5@gmail.com>
2026-01-27 22:43:35 +0530

    Setup in macbook

--- DP/fibonacci.java
+++ DP/fibonacci.java
@@ -14,16 +14,20 @@ public class fibonacci {
     }
 
     public static int fibIter(int n, int f[]){
-        for(int i = 0; i<=n; i++){
-            
+        f[0] = 0;
+        f[1] = 1;
+
+        for(int i = 2; i<=n; i++){
+            f[i] = f[i-1] + f[i-2];
         }
+
         return f[n];
     }
     public static void main(String[] args){
         int n = 6;
         int f[] = new int[n+1];
 
-        System.out.println(fibRecur(n, f));
+        System.out.println(fibIter(n, f));
 
         return;
     }
